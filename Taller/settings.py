c55e83eda116b2e374116043c800dc40965a6841
LauOporto <lauti10oporto@gmail.com>
2024-11-18 21:05:05 -0300

    ayuda

--- Taller/settings.py
+++ Taller/settings.py
@@ -172,7 +172,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 # URLs para archivos estáticos y media
 STATIC_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/static/"
-MEDIA_URL = f'https://{AWS_S3_BUCKET_NAME}.s3.amazonaws.com/'
+MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}.s3.amazonaws.com/'
 
 
 
